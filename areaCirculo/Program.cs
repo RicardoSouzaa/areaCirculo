edea7255edd8dd9d7ef50d74004295b10fef050f
Ricardo Mubarak <psycho696@gmail.com>
2021-03-11 00:34:08 -0300

    Adicionar arquivos de projeto.

---
+++ areaCirculo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace areaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            double area, raio;
+            raio = double.Parse(Console.ReadLine());
+            area = pi * (raio * raio);
+            Console.WriteLine("X = " + Math.Round(area, 4).ToString()); // 3 casas depois da virgula
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ areaCirculo/alternativa1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace areaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            double area, raio;
+            raio = double.Parse(Console.ReadLine());
+            area = pi * (raio * raio);
+            Console.WriteLine($"A={area.ToString("F4")}");
+            Console.ReadLine();
+
+        }
+    }
+}
